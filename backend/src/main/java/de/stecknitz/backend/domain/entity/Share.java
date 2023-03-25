dc50d498bce329d82be3cab97ffa746013c14ee7
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-03-25 16:24:35 +0100

    fix: Share Layer

--- backend/src/main/java/de/stecknitz/backend/domain/entity/Share.java
+++ backend/src/main/java/de/stecknitz/backend/domain/entity/Share.java
@@ -3,9 +3,11 @@ package de.stecknitz.backend.domain.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
+@Table(name = "share")
 @Builder
 @Getter
 @ToString
