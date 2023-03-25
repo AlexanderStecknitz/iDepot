f4fc3a477335bf472467e011b012a272ddfa0826
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-03-25 16:24:14 +0100

    feat: added Depot Entity with all layers

---
+++ backend/src/main/java/de/stecknitz/backend/web/resources/dto/DepotDTO.java
@@ -0,0 +1,9 @@
+package de.stecknitz.backend.web.resources.dto;
+
+import lombok.Data;
+
+@Data
+
+public class DepotDTO {
+    private long id;
+}
---
+++ backend/src/main/java/de/stecknitz/backend/web/resources/dto/mapper/DepotMapper.java
@@ -0,0 +1,11 @@
+package de.stecknitz.backend.web.resources.dto.mapper;
+
+import de.stecknitz.backend.domain.entity.Depot;
+import de.stecknitz.backend.web.resources.dto.DepotDTO;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface DepotMapper {
+    DepotDTO toDepoDto(Depot depot);
+}
