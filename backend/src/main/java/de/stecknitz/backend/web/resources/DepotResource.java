e89785d20ce1044860af15d2e093bf644250be90
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-03-26 16:42:05 +0200

    feat: allowed cors

--- backend/src/main/java/de/stecknitz/backend/web/resources/DepotResource.java
+++ backend/src/main/java/de/stecknitz/backend/web/resources/DepotResource.java
@@ -10,10 +10,7 @@ import de.stecknitz.backend.web.resources.dto.mapper.ShareMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,7 @@ import java.util.List;
 @RequestMapping("/api/depot")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin
 public class DepotResource {
 
     private final DepotService depotService;
--- backend/src/main/java/de/stecknitz/backend/web/resources/ShareResource.java
+++ backend/src/main/java/de/stecknitz/backend/web/resources/ShareResource.java
@@ -7,6 +7,7 @@ import de.stecknitz.backend.web.resources.dto.mapper.ShareMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/share")
 @RequiredArgsConstructor
+@CrossOrigin
 @Slf4j
 public class ShareResource {
 
