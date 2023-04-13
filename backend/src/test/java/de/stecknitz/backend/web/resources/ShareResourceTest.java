6932b607c1d58615e08eecc17e98ae8c1e0e98ac
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-04-13 21:46:54 +0200

    FT-4 feat: depotresource test

---
+++ backend/src/test/java/de/stecknitz/backend/web/resources/ShareResourceTest.java
@@ -0,0 +1,87 @@
+package de.stecknitz.backend.web.resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.stecknitz.backend.core.domain.Share;
+import de.stecknitz.backend.core.service.ShareService;
+import de.stecknitz.backend.web.resources.dto.ShareDTO;
+import de.stecknitz.backend.web.resources.dto.mapper.ShareMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest( ShareResource.class )
+class ShareResourceTest {
+
+    private final static String ENDPOINT = "/api/share";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ShareService shareService;
+
+    @MockBean
+    ShareMapper shareMapper;
+
+    @Test
+    void findAllTest() throws Exception {
+
+        List<Share> shares = List.of(
+                Share.builder()
+                        .isin("TEST1")
+                .build()
+        );
+
+        ShareDTO shareDTO = ShareDTO.builder()
+                .isin("TEST1")
+                .build();
+
+        given( shareService.findAll() ).willReturn(shares);
+        given( shareMapper.toShareDto(shares.get(0)) ).willReturn(shareDTO);
+
+        mockMvc.perform(get(ENDPOINT))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.*", hasSize(1)))
+                .andExpect(jsonPath("$.[0].isin").value("TEST1"))
+                .andReturn();
+
+    }
+
+//    @Test
+//    void createTest() throws Exception {
+//        ShareDTO shareDTO = ShareDTO.builder()
+//                .isin("TEST1")
+//                .build();
+//
+//        Share share = Share.builder()
+//                .isin("TEST1")
+//                .build();
+//
+//        given( shareMapper.toShare(shareDTO) ).willReturn(share);
+//        given( shareService.create(share) ).willReturn(share);
+//
+//        mockMvc.perform(
+//                post(ENDPOINT)
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(TestUtil.convertObjectToJsonBytes(shareDTO))
+//        )
+//                .andExpect(status().isCreated());
+//
+//    }
+
+}
