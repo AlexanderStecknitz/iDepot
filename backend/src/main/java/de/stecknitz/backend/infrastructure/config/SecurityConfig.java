1dcee69e1139a3c0edb4a0a179146298350f4a33
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-07-04 17:02:56 +0200

    feat: added jwt authentication

--- backend/src/main/java/de/stecknitz/backend/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/de/stecknitz/backend/infrastructure/config/SecurityConfig.java
@@ -1,19 +1,34 @@
 package de.stecknitz.backend.infrastructure.config;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import de.stecknitz.backend.core.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration( proxyBeanMethods = false )
 @EnableWebSecurity
@@ -23,19 +38,62 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
-                    authorize.requestMatchers("/api/register").permitAll()
-                            .anyRequest().authenticated()
+                                    authorize.anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return httpSecurity.build();
+    }
 
+    @Bean
+    @Order(1)
+    SecurityFilterChain filterLoginChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .securityMatchers(requestMatcherConfigurer -> requestMatcherConfigurer.requestMatchers("/api/login"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain filterRegisterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .securityMatchers(requestMatcherConfigurer -> requestMatcherConfigurer.requestMatchers("/api/register"))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    RSAKey rsaKey() throws NoSuchAlgorithmException {
+        var generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        var keypair = generator.generateKeyPair();
+        return new RSAKey.Builder((RSAPublicKey) keypair.getPublic())
+                .privateKey(keypair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+
+    @Bean
+    JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+    }
+    @Bean
+    JwtEncoder jwtEncoder(RSAKey rsaKey) {
+        return new NimbusJwtEncoder(
+                new ImmutableJWKSet<>(new JWKSet(rsaKey))
+        );
     }
 
     @Bean
--- backend/src/main/java/de/stecknitz/backend/web/resources/AuthController.java
+++ backend/src/main/java/de/stecknitz/backend/web/resources/AuthController.java
@@ -8,10 +8,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("/api")
@@ -22,11 +29,25 @@ public class AuthController {
     private final UserService userService;
     private final UserMapper userMapper;
 
+    private final JwtEncoder jwtEncoder;
+
     @PostMapping(path = "/register")
     ResponseEntity<Void> register(@RequestBody UserDTO userDTO) {
         User user = userMapper.toUser(userDTO);
         userService.create(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping(path = "/login")
+    public String token(Authentication authentication) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(authentication.getName())
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
 }
