df6da4867222443bcb9307241226b357e8716c14
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-07-08 03:47:43 +0200

    refactor + routing

--- backend/src/main/java/de/stecknitz/backend/web/resources/StockResource.java
+++ backend/src/main/java/de/stecknitz/backend/web/resources/StockResource.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/share")
+@RequestMapping("/api/stock")
 @RequiredArgsConstructor
 @Slf4j
 public class StockResource {
