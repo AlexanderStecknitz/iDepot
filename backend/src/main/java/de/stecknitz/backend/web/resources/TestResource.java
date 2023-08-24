c69ead220acfb71e7f08229d91590efafa9e5775
alexanderstecknitz <Alexander@Stecknitz.eu>
2023-08-24 23:52:14 +0200

    feat: added twelvedata client with end of day data

--- backend/src/main/java/de/stecknitz/backend/web/resources/TestResource.java
+++ backend/src/main/java/de/stecknitz/backend/web/resources/TestResource.java
@@ -1,13 +1,13 @@
 package de.stecknitz.backend.web.resources;
 
-import de.stecknitz.backend.core.service.AlphaVantageService;
+import de.stecknitz.backend.core.service.TwelveDataService;
+import de.stecknitz.backend.core.service.client.twelvedata.dto.EndOfDayDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/test")
@@ -15,14 +15,12 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class TestResource {
 
-    private final AlphaVantageService alphaVantageService;
+    private final TwelveDataService twelveDataService;
 
     @GetMapping
-    public Mono<ResponseEntity<String>> get() {
+    public ResponseEntity<EndOfDayDTO> get() {
         log.debug("get");
-        return alphaVantageService.get()
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+        return ResponseEntity.ok(twelveDataService.get());
     }
 
 }
